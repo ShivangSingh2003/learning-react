10b4b6855283308f0948f54fc97c33083bbb95b0
ShivangSingh2003 <94367469+ShivangSingh2003@users.noreply.github.com>
2024-09-01 20:46:18 +0000

    password generator project

---
+++ 05passwordgenerator/src/App.jsx
@@ -0,0 +1,86 @@
+import { useState, useCallback, useEffect, useRef } from 'react'
+
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [addNums, setAddNums] = useState(false)
+  const [addSplChars, setAddSplChars] = useState(false)
+  const [password, setPassword] = useState("")
+  const [color, setColor] = useState('blue')
+
+  //useRef hook 
+  const passwordRef = useRef(null)
+
+  const copyPassword = useCallback( () => {
+    passwordRef.current?.select()
+    // passwordRef.current?.setSelectionRange(0,4)
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  const passwordGenerator = useCallback(() => {  //memoization...optimization using cache
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if(addNums) str += "0123456789"
+    if(addSplChars) str += "!@#$%^&*()_+-=[]{}|;':,./<>?"
+
+    for (let i = 0; i < length; i++) {
+      let index = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(index)
+      
+    }
+
+    setPassword(pass)
+  }, [length, addNums, addSplChars, setPassword])
+
+  useEffect( ()=> {passwordGenerator()}, [length, addNums, addSplChars, passwordGenerator])
+
+
+
+  return(
+    <>
+      <div className='w-full max-w-3xl mx-auto shadow-md rounded-md px-4 my-12 py-3 text-orange-500 bg-gray-800'>
+        <h1 className='text-white text-center my-3'>Password Generator</h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4 '>
+          <input 
+          type="text"
+          placeholder='password'
+          value={password}
+          readOnly
+          className='outline-none w-full py-3 px-1 mb-3 rounded-md'
+          ref={passwordRef}
+          />
+          <button
+          onClick={() => {copyPassword();setColor('green');}} 
+          className='outline-none h-12 
+           text-white px-3 shrink-0 rounded-md'
+           style={{backgroundColor: color}}
+          >
+          Copy</button>
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input type="range" value={length} min={6} max={50} className='cursor-pointer' 
+             onChange={ (e) => {setLength(e.target.value)} }/>
+            <label htmlFor="range">Length: {length}</label>
+          </div>
+
+          <div className='flex items-center gap-x-1'>
+            <input type="checkbox" defaultChecked={addNums} className='cursor-pointer' 
+             onChange={ () => {setAddNums( (prev) => !prev)} }/>
+            <label htmlFor="checkbox">Add Numbers</label>
+          </div>
+
+          <div className='flex items-center gap-x-1'>
+            <input type="checkbox" defaultChecked={addSplChars} className='cursor-pointer' 
+             onChange={ () => {setAddSplChars( (prev) => !prev)} }/>
+            <label htmlFor="checkbox">Add Special Characters</label>
+          </div>
+
+        </div>
+      </div> 
+    </>
+  )
+}
+
+export default App
