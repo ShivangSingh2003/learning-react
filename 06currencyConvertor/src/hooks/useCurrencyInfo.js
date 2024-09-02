a6baa4ff8db66aedc743868e6202ef42d53fa75f
ShivangSingh2003 <94367469+ShivangSingh2003@users.noreply.github.com>
2024-09-02 19:39:12 +0000

    currency convertor project

---
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react"
+
+function useCurrencyInfo(currency){
+    const[data, setData] = useState({})
+    useState( () => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        // https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json
+        .then( (res) => res.json() )
+        .then( (res) => setData(res[currency]) )
+
+        console.log(data);
+        
+    }, [currency] )
+
+    console.log(data);
+    return data
+    
+} 
+
+export default useCurrencyInfo
