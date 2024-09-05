2990f74cbfbd80d10756736b0b27376a767dc99c
ShivangSingh2003 <94367469+ShivangSingh2003@users.noreply.github.com>
2024-09-05 17:13:11 +0000

    added user context

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,5 +1,7 @@
 import UserContextProvider from '../context/UserContextProvider'
 import './App.css'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
 
 function App() {
 
@@ -7,6 +9,8 @@ function App() {
   return (
     <UserContextProvider>
       <h1>React with Context</h1>
+      <Login />
+      <Profile />
     </UserContextProvider>
   )
 }
--- 08miniContext/src/Components/Login.jsx
+++ 08miniContext/src/Components/Login.jsx
@@ -8,7 +8,7 @@ function Login() {
     const {setUser} = useContext(UserContext)
     const handleSubmit = (e) => {
         e.preventDefault()
-        setUser({username, password})
+        setUser({username, password })
     }
     return(
         <div>
